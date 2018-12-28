f8aa098521046118a6d6b86f38745d7bb871d250
Michael Læntver <brormand_96@hotmail.com>
2018-12-29 00:45:22 +0100

    Launch

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael
+ * Date: 21/11/2018
+ * Time: 01:44
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+
+</body>
+</html>
