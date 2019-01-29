d497ff16739d53e62a7f28e78d2149ff00bc72e8
Michael Læntver <brormand_96@hotmail.com>
2019-01-29 02:54:12 +0100

    Backend renovation

--- api/objects/project.php
+++ api/objects/project.php
@@ -10,10 +10,217 @@ class Project {
 	public $name;
 	public $description;
 	public $created;
+	public $user_id;
 	public $user;
 
+	public $search;
+
 	// constructor with $db as database connection
-	public function __construct($db){
+	public function __construct( $db ) {
 		$this->conn = $db;
+	}
+
+	// create article
+	function create() {
+		// query to insert record
+		$query = "INSERT INTO " . $this->table_name . " (name, description, fk_user_id) 
+					VALUES (:name, :description, :user_id)";
+
+		// prepare query
+		$stmt = $this->conn->prepare( $query );
+
+		// sanitize
+		$this->name        = htmlspecialchars( strip_tags( $this->name ) );
+		$this->description = htmlspecialchars( strip_tags( $this->description ) );
+		$this->user_id     = htmlspecialchars( strip_tags( $this->user_id ) );
+
+		// bind values
+		$stmt->bindParam( ":name", $this->name, PDO::PARAM_STR );
+		$stmt->bindParam( ":description", $this->description, PDO::PARAM_STR );
+		$stmt->bindParam( ":user_id", $this->user_id, PDO::PARAM_INT );
+
+		// execute query
+		if ( $stmt->execute() )
+		{
+			return true;
+		}
+
+		return false;
+
+	}
+
+	// read articles
+	function read() {
+		// select all query
+		$query = "SELECT a.id as article_id, a.name as article, a.tray_number, a.barcode, a.on_loan, a.quantity, a.fk_category_id as category_id, c.name as category
+				FROM " . $this->table_name . " a
+				INNER JOIN categories c ON c.id = a.fk_category_id";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+
+		// execute query
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	// search articles
+	function search( $keywords ) {
+
+		// select all query
+		$query = "SELECT a.id as article_id, a.name as article, a.tray_number, a.barcode, a.on_loan, a.quantity, a.fk_category_id as category_id, c.name as category
+				FROM " . $this->table_name . " a
+				INNER JOIN categories c ON c.id = a.fk_category_id
+				WHERE a.name LIKE ? OR a.barcode LIKE ? OR a.tray_number LIKE ? OR c.name LIKE ?";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+
+		// sanitize
+		$keywords = htmlspecialchars( strip_tags( $keywords ) );
+		$keywords = "%{$keywords}%";
+
+		// bind
+		$stmt->bindParam( 1, $keywords );
+		$stmt->bindParam( 2, $keywords );
+		$stmt->bindParam( 3, $keywords );
+		$stmt->bindParam( 4, $keywords );
+
+		// execute query
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	// read one article
+	function readOne() {
+		// update query
+		$query = "SELECT a.id as article_id, a.name as article, a.tray_number, a.barcode, a.on_loan, a.quantity, a.fk_category_id as category_id, c.name as category
+				FROM " . $this->table_name . " a
+				INNER JOIN categories c ON c.id = a.fk_category_id
+				WHERE barcode = :barcode";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+
+		$this->barcode = htmlspecialchars( strip_tags( $this->barcode ) );
+
+		// bind values
+		$stmt->bindParam( ":barcode", $this->barcode, PDO::PARAM_STR );
+
+		// execute query
+		$stmt->execute();
+
+		// get retrieved row
+		$row = $stmt->fetch( PDO::FETCH_ASSOC );
+
+		// set values to object properties
+		$this->id          = $row['article_id'];
+		$this->name        = $row['article'];
+		$this->tray_number = $row['tray_number'];
+		$this->barcode     = $row['barcode'];
+		$this->on_loan     = $row['on_loan'];
+		$this->quantity    = $row['quantity'];
+		$this->category_id = $row['category_id'];
+		$this->category    = $row['category'];
+
+		return $stmt;
+	}
+
+	// search articles
+	function readPaging( $from_record_num, $records_per_page ) {
+
+		// select all query
+		$query = "SELECT a.id as article_id, a.name as article, a.tray_number, a.barcode, a.on_loan, a.quantity, a.fk_category_id as category_id, c.name as category
+				FROM " . $this->table_name . " a
+				INNER JOIN categories c ON c.id = a.fk_category_id
+				LIMIT ?, ?";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+
+		// bind
+		$stmt->bindParam( 1, $from_record_num );
+		$stmt->bindParam( 2, $records_per_page );
+
+		// execute query
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	// update articles
+	function update() {
+		// select all query
+		$query = "UPDATE " . $this->table_name .
+		         " SET name = :name, tray_number = :tray_number, barcode = :barcode, quantity = :quantity, fk_category_id = :category_id WHERE ID = :id";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+
+		// sanitize
+		$this->id          = htmlspecialchars( strip_tags( $this->id ) );
+		$this->name        = htmlspecialchars( strip_tags( $this->name ) );
+		$this->tray_number = htmlspecialchars( strip_tags( $this->tray_number ) );
+		$this->barcode     = htmlspecialchars( strip_tags( $this->barcode ) );
+		$this->on_loan     = htmlspecialchars( strip_tags( $this->on_loan ) );
+		$this->quantity    = htmlspecialchars( strip_tags( $this->quantity ) );
+		$this->category_id = htmlspecialchars( strip_tags( $this->category_id ) );
+
+		// bind values
+		$stmt->bindParam( ":id", $this->id, PDO::PARAM_INT );
+		$stmt->bindParam( ":name", $this->name, PDO::PARAM_STR );
+		$stmt->bindParam( ":tray_number", $this->tray_number, PDO::PARAM_INT );
+		$stmt->bindParam( ":barcode", $this->barcode, PDO::PARAM_STR );
+		$stmt->bindParam( ":quantity", $this->quantity, PDO::PARAM_INT );
+		$stmt->bindParam( ":category_id", $this->category_id, PDO::PARAM_INT );
+
+		// execute the query
+		if ( $stmt->execute() )
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	// delete article
+	function delete() {
+		// delete query
+		$query = "DELETE FROM " . $this->table_name . " WHERE id = :id";
+
+		// prepare query
+		$stmt = $this->conn->prepare( $query );
+
+		// sanitize
+		$this->id = htmlspecialchars( strip_tags( $this->id ) );
+
+		// bind id of record to delete
+		$stmt->bindParam( ":id", $this->id, PDO::PARAM_INT );
+
+		// execute query
+		if ( $stmt->execute() )
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	// delete article
+	function count() {
+		// delete query
+		$query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name;
+
+		// prepare query
+		$stmt = $this->conn->prepare( $query );
+
+		// execute query
+		$stmt->execute();
+
+		$row = $stmt->fetch( PDO::FETCH_ASSOC );
+
+		return $row["total_rows"];
 	}
 }
--- api/project/create.php
+++ api/project/create.php
@@ -1,19 +1,59 @@
 <?php
 session_start();
-require_once "database1.php";
+// required headers
+header( "Access-Control-Allow-Origin: *" );
+header( "Content-Type: application/json; charset=UTF-8" );
+header( "Access-Control-Allow-Methods: POST" );
+header( "Access-Control-Max-Age: 3600" );
+header( "Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" );
 
-try {
-	if ( isset( $_POST["projekt"] ) && isset( $_POST["beskrivelse"] ) && isset( $_SESSION["userid"] )) {
-		$sql = "INSERT INTO projekter (Navn, Beskrivelse, FK_bruger_ID) 
-					VALUES (:navn, :beskrivelse, :bruger)";
-		$sth = $conn->prepare( $sql );
-		$sth->bindParam( ':navn', $_POST["projekt"], PDO::PARAM_STR );
-		$sth->bindParam( ':beskrivelse', $_POST["beskrivelse"], PDO::PARAM_STR );
-		$sth->bindParam( ':bruger', $_SESSION["userid"], PDO::PARAM_INT );
-		$sth->execute();
+if ( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest' )
+{
+	// include database and object files
+	require_once '../config/database.php';
+	require_once '../objects/project.php';
+
+	// initialize object
+	$project = new Project( $conn );
+
+	// get posted data
+	//$data = json_decode( file_get_contents( "php://input" ) );
+
+	if ( isset( $_POST["project"] ) && isset( $_POST["description"] ) && isset( $_SESSION["USER_ID"] ) )
+	{
+		// set user property values
+		$project->name        = $_POST['project'];
+		$project->description = $_POST['description'];
+		$project->user_id     = $_SESSION["USER_ID"];
+
+		// create the user
+		if ( $project->create() )
+		{
+			// tell the user
+			echo json_encode( array( "message" => "Projektet er oprettet" ) );
+
+			// set response code - 201 created
+			http_response_code( 201 );
+		}
+		// if unable to create the user, tell the user
+		else
+		{
+			// set response code - 503 service unavailable
+			http_response_code( 503 );
+
+			// tell the user
+			die( json_encode( array( "message" => "Fejl under oprettelse af projekt" ) ) );
+		}
+	}
+	// tell the user data is incomplete
+	else
+	{
+		// set response code - 400 bad request
+		http_response_code( 400 );
+
+		// tell the user
+		die( json_encode( array( "message" => "Unable to create user. Data is incomplete." ) ) );
 	}
 }
-catch( PDOException $e ) {
-	echo $e->getMessage();
-}
+
 ?>
